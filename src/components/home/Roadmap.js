570e62ea6ff9533461af105621e955ddc1b9d57a
WahyuS002 <syahputrawahyu61@gmail.com>
2022-01-26 08:44:18 +0700

    :zap: update roadmap checklist

--- src/components/home/Roadmap.js
+++ src/components/home/Roadmap.js
@@ -25,7 +25,11 @@ function Roadmap() {
                 <div className="mb-8 flex justify-between items-center w-full">
                     <div className="hidden md:block w-5/12"></div>
                     <div className="z-20">
-                        <div className="bg-semi-black w-8 h-8 rounded-full"></div>
+                        <div className="flex items-center justify-center bg-primary text-white w-8 h-8 rounded-full">
+                            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+                            </svg>
+                        </div>
                     </div>
                     <div className="bg-semi-black shadow-2xl shadow-gray-400/10 rounded-xl  w-full md:w-5/12 p-6 md:px-8 md:py-6 ml-8 md:ml-0">
                         <h3 className="font-semibold text-base md:text-lg">Open presale at 7A for the first 50 OG members AKA</h3>
@@ -35,7 +39,11 @@ function Roadmap() {
                 <div className="mb-8 flex justify-between items-center w-full md:flex-row-reverse">
                     <div className="hidden md:block w-5/12"></div>
                     <div className="z-20">
-                        <div className="bg-semi-black w-8 h-8 rounded-full"></div>
+                        <div className="flex items-center justify-center bg-primary text-white w-8 h-8 rounded-full">
+                            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+                            </svg>
+                        </div>
                     </div>
                     <div className="bg-semi-black shadow-2xl shadow-gray-400/10 rounded-xl  w-full md:w-5/12 p-6 md:px-8 md:py-6 ml-8 md:ml-0">
                         <h3 className="font-semibold text-base md:text-lg">Partnerships and collaborations</h3>
@@ -47,7 +55,11 @@ function Roadmap() {
                 <div className="mb-8 flex justify-between items-center w-full">
                     <div className="hidden md:block w-5/12"></div>
                     <div className="z-20">
-                        <div className="bg-semi-black w-8 h-8 rounded-full"></div>
+                        <div className="flex items-center justify-center bg-primary text-white w-8 h-8 rounded-full">
+                            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+                            </svg>
+                        </div>
                     </div>
                     <div className="bg-semi-black shadow-2xl shadow-gray-400/10 rounded-xl  w-full md:w-5/12 p-6 md:px-8 md:py-6 ml-8 md:ml-0">
                         <h3 className="font-semibold text-base md:text-lg">150 Apes will be sold on shuffles in the discord for 10 Algo</h3>
@@ -60,23 +72,31 @@ function Roadmap() {
                 <div className="mb-8 flex justify-between items-center w-full md:flex-row-reverse">
                     <div className="hidden md:block w-5/12"></div>
                     <div className="z-20">
-                        <div className="bg-semi-black w-8 h-8 rounded-full"></div>
+                        <div className="flex items-center justify-center bg-primary text-white w-8 h-8 rounded-full">
+                            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+                            </svg>
+                        </div>
                     </div>
                     <div className="bg-semi-black shadow-2xl shadow-gray-400/10 rounded-xl  w-full md:w-5/12 p-6 md:px-8 md:py-6 ml-8 md:ml-0">
                         <h3 className="font-semibold text-base md:text-lg">The Tiny Ape Treasury for Monkey Business</h3>
                         <p className="mt-3 leading-relaxed tracking-wide text-gray-500 text-sm md:text-base">
                             50% of TOTAL SALES will go to the Treasury for Monkey Business. A DAO will be established for decisions on how to invest the funds such as: for the support of other
-                            projects, to invest in blue-chip NFT projects, to invest in promising ASA’s/Tokens, etc.
+                            projects, to invest in blue-chip NFT projects, to invest in promising ASA's/Tokens, etc.
                         </p>
                     </div>
                 </div>
                 <div className="mb-8 flex justify-between items-center w-full">
                     <div className="hidden md:block w-5/12"></div>
                     <div className="z-20">
-                        <div className="bg-semi-black w-8 h-8 rounded-full"></div>
+                        <div className="flex items-center justify-center bg-primary text-white w-8 h-8 rounded-full">
+                            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+                            </svg>
+                        </div>
                     </div>
                     <div className="bg-semi-black shadow-2xl shadow-gray-400/10 rounded-xl  w-full md:w-5/12 p-6 md:px-8 md:py-6 ml-8 md:ml-0">
-                        <h3 className="font-semibold text-base md:text-lg">NFT Community Valt</h3>
+                        <h3 className="font-semibold text-base md:text-lg">NFT Community Vault</h3>
                         <p className="mt-3 leading-relaxed tracking-wide text-gray-500 text-sm md:text-base">
                             We will begin to Accept Donations of promising NFTs from upcoming artists as well as pieces from Blue Chip NFT projects to put towards a community vault of NFTs, the
                             community will then decide what NFTs we will accept and for how long to hold them.
@@ -86,12 +106,16 @@ function Roadmap() {
                 <div className="mb-8 flex justify-between items-center w-full md:flex-row-reverse">
                     <div className="hidden md:block w-5/12"></div>
                     <div className="z-20">
-                        <div className="bg-semi-black w-8 h-8 rounded-full"></div>
+                        <div className="flex items-center justify-center bg-primary text-white w-8 h-8 rounded-full">
+                            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7"></path>
+                            </svg>
+                        </div>
                     </div>
                     <div className="bg-semi-black shadow-2xl shadow-gray-400/10 rounded-xl  w-full md:w-5/12 p-6 md:px-8 md:py-6 ml-8 md:ml-0">
                         <h3 className="font-semibold text-base md:text-lg">"Tiny Apes saves the Jungles” Charity program</h3>
                         <p className="mt-3 leading-relaxed tracking-wide text-gray-500 text-sm md:text-base">
-                            The Tiny Apes might have left the Jungle but that doesn’t mean that they’ve forgotten their roots! As members of the Algorand blockchain, we take pride in our
+                            The Tiny Apes might have left the Jungle but that doesn't mean that they've forgotten their roots! As members of the Algorand blockchain, we take pride in our
                             carbon-negative blockchain! The Algorand and the Tiny Ape army loves this Planet and its beautiful creatures; therefore 10% of TOTAL SALES will be donated to the Jane
                             Goodall Institute
                         </p>
